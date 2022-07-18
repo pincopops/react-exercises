0f0aa559130ab615510754f15fd2d6293f36c9d5
Elio Scaletta <elioscal@hotmail.it>
2022-07-18 19:29:32 +0200

    Competamento props - 01.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Hello } from './Hello';
+import { Welcome } from './Welcome';
  
 export class App extends React.Component{
     render(){
-        return <div><Hello /></div>
+        return <div>
+                <Hello />
+                <Welcome name='Elio' />
+            </div>
     }
 }
 
