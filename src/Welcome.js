7724c5e2fdbcb9d84ae96aeef5d8defb075fa5fc
Elio Scaletta <elioscal@hotmail.it>
2022-07-18 19:55:43 +0200

    Completamento props - 02.

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,8 +2,15 @@ import React from "react";
 
 
 export class Welcome extends React.Component{
+
+    //setting default value for name - conditional rendering
     render(){
-        return <p>Hello, {this.props.name}!</p>
+        if(this.props.name){
+            return <p>Hello, {this.props.name}!</p>
+        } else{
+            return 'Hello, User!';
+        }
+
     }
 }
 
