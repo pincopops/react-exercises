a9cf658615ca310abb7584b35c5df91ebe903f54
Elio Scaletta <elioscal@hotmail.it>
2022-07-19 12:39:39 +0200

    Completamento conditional-rendering-02.

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,7 +3,9 @@ import { Age } from "./Age";
 
 
 export class Welcome extends React.Component{
-
+//soluzione dell'esercizio conditional-rendering-02 già implementata nei
+//branch precedenti. Age non viene mostrato se non passato come prop o se
+//non corrispondente a maggiore di 18.
     render(){
         if(this.props.name && this.props.age > 18){
             return <div>
