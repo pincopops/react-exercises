5a513b7b56e93fb77f1e92d3af2fc1cd054dd246
Elio Scaletta <elioscal@hotmail.it>
2022-08-22 10:16:47 +0200

    Completamento styling-components-03.

--- src/Login.js
+++ src/Login.js
@@ -33,14 +33,17 @@ export class Login extends React.Component{
             remember: false,
         })
     }
-
     render(){
+        const myLoginButtonCss = {
+            backgroundColor: this.state.password.length < 8 ? "red" : "green",
+        }
+
         return <div>
             <h3>This is a controlled login form</h3>
             <input type="text" name="username" value={this.state.username} onChange={this.handleInputChanges}></input>
             <input type="password" name="password" value={this.state.password} onChange={this.handleInputChanges}></input>
             <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleInputChanges}></input>
-            <button disabled={!(this.state.username && this.state.password)} onClick={this.handleButtonClick}>Login</button>
+            <button style={myLoginButtonCss} disabled={!(this.state.username && this.state.password)} onClick={this.handleButtonClick}>Login</button>
             <button onClick={this.handleResetButton}>Reset</button>
         </div>
     }
