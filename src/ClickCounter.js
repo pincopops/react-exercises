4f2be85bb090ee2f0188eca82730323e76c70847
Elio Scaletta <elioscal@hotmail.it>
2022-08-25 16:42:40 +0200

    Completamento useState-01.

--- src/ClickCounter.js
+++ src/ClickCounter.js
@@ -1,32 +1,41 @@
-import React from 'react';
+import { useState } from "react";
 
-//Create a ClickCounter class component that increments a counter every time a user clicks on a button. Render both the current value of the counter and the button within the ClickCounter component.
-
-export class ClickCounter extends React.Component{
+export function ClickCounter ({initialValue = 0}) {
+    const [counter, setCounter] = useState(initialValue)
     
-    state = {
-        count: this.props.initialStateValue,
+    function handleCounterClick(){
+        setCounter((counter) => counter + 1)
     }
-
-    handleCounterClick = () => {
-        this.setState((state) => {
-            return{
-                count: state.count + this.props.incrementedBy,   
-            } 
-        })
-
-    }
-
-    render(){
-        return <div>
-            <button onClick={this.handleCounterClick}>Click to increment</button>
-            <div>{this.state.count}</div>
-        </div>
-    }
+    return <div>
+        <button onClick={handleCounterClick}>Click to increment</button>
+        <span style={{backgroundColor: "red", color: "white", borderRadius: "2rem"}}><strong>{counter}</strong></span>
+    </div>
 }
+// export class ClickCounter extends React.Component{
+    
+//     state = {
+//         count: this.props.initialStateValue,
+//     }
 
-ClickCounter.defaultProps = {
-    initialStateValue: 0,
-    incrementedBy: 1,
+//     handleCounterClick = () => {
+//         this.setState((state) => {
+//             return{
+//                 count: state.count + this.props.incrementedBy,   
+//             } 
+//         })
 
-}
+//     }
+
+//     render(){
+//         return <div>
+//             <button onClick={this.handleCounterClick}>Click to increment</button>
+//             <div>{this.state.count}</div>
+//         </div>
+//     }
+// }
+
+// ClickCounter.defaultProps = {
+//     initialStateValue: 0,
+//     incrementedBy: 1,
+
+// }
