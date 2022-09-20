c81b518bc69e1f8c9c40abb2ee43c2dda42cd25e
Elio Scaletta <elioscal@hotmail.it>
2022-09-20 11:31:58 +0200

    Completamento custom-hooks-04.

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react"
 import { useGithubUser } from "./custom hooks/useGithubUser"
 
 export function GithubUser({ username }) {
-    const { data } = useGithubUser({ username });
+    const { data, loading, error } = useGithubUser({ username });
 
     return (
         <div>
             <h1>This is the owner's name of the fetched Github account:</h1>
+            {loading && <span>LOADING...</span>}
+            {error && <span>Something went wrong: user doesn't exist</span>}
             {data && <span>{data.name}</span>}
             <h2>It was created on:</h2>
             {data && <span>{data.created_at}</span>}
--- src/custom hooks/useGithubUser.js
+++ src/custom hooks/useGithubUser.js
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react"
 
 export function useGithubUser({ username }) {
-    const [data, setData] = useState(null)
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    useEffect(() => {
+    const useEffectConst = useEffect(() => {
+        setLoading(true)
+        setError(null)
         fetch(`https://api.github.com/users/${username}`)
             .then((response) => {
-                return response.json()
+                if (response.status !== 200) {
+                    setError(new Error("User doesn't exist."))
+                } else return response.json()
             })
             .then((json) => {
                 console.log(json)
+                setLoading(false)
                 setData(json)
+            })
+            .catch((error) => {
+                setError(error)
+            })
+            .finally(() => {
+                setLoading(false)
             })
     }, [username])
 
     return {
         data: data,
+        loading: loading,
+        error: error,
+        useEffect: useEffectConst,
     }
 }
