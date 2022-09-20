2ad67e3fef0a3f8b50e60828188e1d926f236300
Elio Scaletta <elioscal@hotmail.it>
2022-09-20 15:06:17 +0200

    Completamento useCallback.

--- src/custom hooks/useCounter.js
+++ src/custom hooks/useCounter.js
@@ -1,19 +1,20 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-export function useCounter({initialValue = 0}) {
+export function useCounter({ initialValue = 0 }) {
     const [counter, setCounter] = useState(initialValue)
 
-    function handleCounterIncrement(){
+    const handleCounterIncrement = useCallback(function handleCounterIncrement() {
         setCounter((c) => c + 1)
-    }
-    
-    function handleCounterDecrement(){
+    }, [])
+
+    const handleCounterDecrement = useCallback(function handleCounterDecrement() {
         setCounter((c) => c - 1)
-    }
-    
-    function handleCounterReset(){
+    }, [])
+
+    const handleCounterReset = useCallback(function handleCounterReset() {
         setCounter(initialValue)
-    }
+    }, [initialValue])
+
 
     return {
         counter: counter,
